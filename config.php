ce1110bb860ef71d7ae87a2e15ff94f1dbe45fb8
Nitzan <nitzanb@spotoption.com>
2016-05-01 09:18:49 +0300

    Adding local test file on the server

--- config.php
+++ config.php
@@ -1,7 +1,12 @@
 <?php
 
-define("TOKEN", "devTest");
-define("AESKey", 'f09sfd0s8d0f0sdudf0sdjf0s89f0usdf9s0dfu0sdf');
-define("appId", "");
-define("appSecret","");
+if(file_exists(ABSPATH.'/devtest.php'))
+	include(ABSPATH.'/config.php');
+else
+{
+	define("TOKEN", "devTest");
+	define("AESKey", 'f09sfd0s8d0f0sdudf0sdjf0s89f0usdf9s0dfu0sdf');
+	define("appId", "");
+	define("appSecret","");	
+}
 
